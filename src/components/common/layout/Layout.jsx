5dc6998e1d1e8d1cd8e2c728548cb5ca9943362b
shower510 <shower510@naver.com>
2023-11-24 13:12:08 +0900

    useSplittext이용해서제목뜨게만들기

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -4,23 +4,19 @@ import { useSplitText } from '../../../hooks/useSplitText';
 
 export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
-	//useEffect안쪽에서 자주 쓰일만한 특정 함수를 호출해야됨
-	//use로 시작하는 커스텀훅은 특정함수 안쪽에서 호출불가
-	//해당 hok이 함수를 반환하도록 처리
+	const refTitle = useRef(null);
+
 	const splitText = useSplitText();
-	console.log(splitText);
 
 	useEffect(() => {
-		//아래처럼 custom hook이 반환한 함수를 hook이나 핸들러함수 내부에서 가능
+		splitText(refTitle.current, title);
 		refFrame.current.classList.add('on');
 	}, []);
 
 	return (
 		<main ref={refFrame} className={`Layout ${title}`}>
-			<h1>{title}</h1>
+			<h1 ref={refTitle}>{title}</h1>
 			<div className='bar'></div>
-			{/* children 복사할거에 해두고
-      복사할위치에 <Layout>내용</Layout>감싸면 레이아웃도 복사됨  */}
 			{children}
 		</main>
 	);
